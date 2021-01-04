6bf4b9d62f1061a88100f2ee98de68aa9fb448a3
BX32_Brushless <37931458+conuthead@users.noreply.github.com>
2021-01-04 11:18:27 -0400

    added pwm enable bridge support

--- Core/Inc/targets.h
+++ Core/Inc/targets.h
@@ -1,4 +1,5 @@
 #define G072ESC
+//#define G071ENABLE
 
 //GLOBAL
 //#define USE_ADC_INPUT
@@ -36,7 +37,39 @@
 #endif
 
 
+#ifdef G071ENABLE
 
+#define    FIRMWARE_NAME  "G071 PWM_EN "
+
+#define    PWM_ENABLE_BRIDGE
+#define    USE_TIMER_3_CHANNEL_1
+
+#define    INPUT_PIN               LL_GPIO_PIN_4
+#define    INPUT_PIN_PORT              GPIOB
+#define    IC_TIMER_CHANNEL         LL_TIM_CHANNEL_CH1
+#define    IC_TIMER_REGISTER          TIM3
+#define    IC_TIMER_POINTER           htim3
+
+#define    INPUT_DMA_CHANNEL       LL_DMA_CHANNEL_1
+#define    DMA_HANDLE_TYPE_DEF     hdma_tim3_ch1
+#define    IC_DMA_IRQ_NAME         DMA1_Channel1_IRQn
+
+#define PHASE_A_GPIO_PWM          LL_GPIO_PIN_10
+#define PHASE_A_GPIO_PORT_PWM         GPIOA
+#define PHASE_A_GPIO_ENABLE          LL_GPIO_PIN_1
+#define PHASE_A_GPIO_PORT_ENABLE         GPIOB
+
+#define PHASE_B_GPIO_PWM          LL_GPIO_PIN_9
+#define PHASE_B_GPIO_PORT_PWM         GPIOA
+#define PHASE_B_GPIO_ENABLE          LL_GPIO_PIN_0
+#define PHASE_B_GPIO_PORT_ENABLE         GPIOB
+
+#define PHASE_C_GPIO_PWM          LL_GPIO_PIN_8
+#define PHASE_C_GPIO_PORT_PWM         GPIOA
+#define PHASE_C_GPIO_ENABLE          LL_GPIO_PIN_7
+#define PHASE_C_GPIO_PORT_ENABLE         GPIOA
+
+#endif
 
 
 
